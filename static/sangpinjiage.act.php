1cb21a0d4d3a4e9722e540ed69e3b1cf4950879e
root <root@ipsan.storage>
2015-11-18 23:18:10 +0800

    发布任务提交

--- static/sangpinjiage.act.php
+++ static/sangpinjiage.act.php
@@ -1,18 +1,35 @@
-<?php
-$p=trim($_GET["p"]);
-$url='http://www.fan800.net.cn/mod/zhuaqu/sangpin.act.php?p='.$p;
-$content=curl_file_get_contents($url);
-echo $content;
-function curl_file_get_contents($durl){ 
-$cookie_file = dirname(__FILE__)."/cookie.txt"; 
-$ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, $durl); 
-curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); 
-curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
-$r = curl_exec($ch); 
-curl_close($ch); 
-return $r; 
-}
+<?php
+
+$p=trim($_GET["p"]);
+
+$url='http://www.fan800.org/mod/zhuaqu/sangpin.act.php?p='.$p;
+
+$content=curl_file_get_contents($url);
+
+echo $content;
+
+function curl_file_get_contents($durl){ 
+
+$cookie_file = dirname(__FILE__)."/cookie.txt"; 
+
+$ch = curl_init(); 
+
+curl_setopt($ch, CURLOPT_URL, $durl); 
+
+curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); 
+
+curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file); 
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
+
+$r = curl_exec($ch); 
+
+curl_close($ch); 
+
+return $r; 
+
+}
+
 ?>
